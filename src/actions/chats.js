2854539e789325f09461f9d252ac55f57971ae55
Anatoly Shlom <a.m.shlom@gmail.com>
2018-10-28 16:08:42 +0300

    Avoiding race conditions

--- src/actions/chats.js
+++ src/actions/chats.js
@@ -4,7 +4,13 @@ import { redirect } from './services';
 
 export function fetchAllChats() {
   return (dispatch, getState) => {
-    const { token } = getState().auth;
+    const state = getState();
+    const { isFetching } = state.services;
+    const { token } = state.auth;
+
+    if (isFetching.allChats) {
+      return Promise.resolve();
+    }
     dispatch({
       type: types.FETCH_ALL_CHATS_REQUEST
     })
@@ -23,7 +29,13 @@ export function fetchAllChats() {
 
 export function fetchMyChats() {
   return (dispatch, getState) => {
-    const { token } = getState().auth;
+    const state = getState();
+    const { isFetching } = state.services;
+    const { token } = state.auth;
+
+    if (isFetching.myChats) {
+      return Promise.resolve();
+    }
 
     dispatch({
       type: types.FETCH_MY_CHATS_REQUEST
@@ -43,8 +55,13 @@ export function fetchMyChats() {
 
 export function fetchChat(chatId) {
   return (dispatch, getState) => {
-    const { token } = getState().auth;
+    const state = getState();
+    const { isFetching } = state.services;
+    const { token } = state.auth;
 
+    if (isFetching.chat) {
+      return Promise.resolve();
+    }
     dispatch({
       type: types.FETCH_CHAT_REQUEST
     });
@@ -83,14 +100,21 @@ export function setActiveChat(chatId) {
           type: types.SET_ACTIVE_CHAT,
           payload: data,
         });
+        return dispatch(redirect(`/chat/${data.chat._id}`));
+
       });
   };
 }
 
 export function createChat(title) {
   return (dispatch, getState) => {
-    const { token } = getState().auth;
+    const state = getState();
+    const { isFetching } = state.services;
+    const { token } = state.auth;
 
+    if (isFetching.createChat) {
+      return Promise.resolve();
+    }
     dispatch({
       type: types.CREATE_CHAT_REQUEST,
       payload: { title }
@@ -118,8 +142,13 @@ export function createChat(title) {
 
 export function joinChat(chatId) {
   return (dispatch, getState) => {
-    const { token } = getState().auth;
+    const state = getState();
+    const { isFetching } = state.services;
+    const { token } = state.auth;
 
+    if (isFetching.joinChat) {
+      return Promise.resolve();
+    }
     dispatch({
       type: types.JOIN_CHAT_REQUEST,
       payload: { chatId }
@@ -145,8 +174,13 @@ export function joinChat(chatId) {
 
 export function leaveChat(chatId) {
   return (dispatch, getState) => {
-    const { token } = getState().auth;
+    const state = getState();
+    const { isFetching } = state.services;
+    const { token } = state.auth;
 
+    if (isFetching.leaveChat) {
+      return Promise.resolve();
+    }
     dispatch({
       type: types.LEAVE_CHAT_REQUEST,
       payload: { chatId }
@@ -176,8 +210,13 @@ export function leaveChat(chatId) {
 
 export function deleteChat(chatId) {
   return (dispatch, getState) => {
-    const { token } = getState().auth;
+    const state = getState();
+    const { isFetching } = state.services;
+    const { token } = state.auth;
 
+    if (isFetching.deleteChat) {
+      return Promise.resolve();
+    }
     dispatch({
       type: types.DELETE_CHAT_REQUEST,
       payload: { chatId }
